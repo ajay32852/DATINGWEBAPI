7d617fefa10118777794bfb93914537bc23e1f24
Ajay Kumar <ajay32852@gmail.com>
2025-06-03 18:03:03 +0530

    Login Files Commit code

---
+++ DATINGWEBAPI.DTO/DTOs/UserInterestDTO.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DATINGWEBAPI.DTO.DTOs
+{
+    public class UserInterestDTO
+    {
+        public long UserInterestId { get; set; }
+        public long UserId { get; set; }
+        public long InterestId { get; set; }
+    }
+}
---
+++ DATINGWEBAPI.DTO/DTOs/VerificationCodeDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace DATINGWEBAPI.DTO.DTOs
+{
+    public class VerificationCodeDTO
+    {
+        public long Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Code { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool IsUsed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public long? UserId { get; set; }
+        public virtual UserDTO? User { get; set; }
+    }
+
+}
